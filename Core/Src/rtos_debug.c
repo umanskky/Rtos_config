81e38efc5d4f9593f3399f5d0001b2eb72ec6f03
umanskky <umanskky@gmail.com>
2021-11-15 22:10:58 +0300

    Сделай Task_Create!

--- Core/Src/rtos_debug.c
+++ Core/Src/rtos_debug.c
@@ -4,10 +4,9 @@
 
 //*** defines****
 
-#define  MEMPOOL_OBJECTS 16  
+#define  MSGQUEUE_OBJECTS 16  
 
 //*** Task *****
-
 osThreadId_t Task1;
 osThreadId_t Task2;
 
@@ -18,24 +17,42 @@ osTimerId_t tim_periodic;
 osSemaphoreId_t sem_tim;
 
 //*** Memory *****
-
 /*
  char *path = pvPortMalloc(20*sizeof (char));
  vPortFree(path);
 */
 
+//*** Queue ******
+
+osMessageQueueId_t que_id;
+
+//****************
+
 uint16_t size;
+
+#pragma pack(push, 1)
+
+typedef struct{
+	
+	uint8_t a;
+	uint16_t b;
+	uint8_t buff[1];
+	
+		
+}MY_STRUCT;
+
+#pragma pack(pop)
 
 //*****************************************
 const osThreadAttr_t Task1_attributes = {
   .name = "defaultTask",
-  .priority = (osPriority_t) osPriorityNormal,
+  .priority = (osPriority_t) osPriorityHigh,
   .stack_size = 1024
 };
 
 const osThreadAttr_t Task2_attributes = {
   .name = "defaultTask_2",
-  .priority = (osPriority_t) osPriorityNormal,
+  .priority = (osPriority_t) osPriorityNormal, //osPriorityHigh
   .stack_size = 1024
 };
 
@@ -48,7 +65,7 @@ void Task_init(void)
   Task2 = osThreadNew(StartTask2, NULL, &Task2_attributes);
   tim_periodic = osTimerNew(periodic_Callback, osTimerPeriodic,(void*) 0, NULL );
   sem_tim = osSemaphoreNew(1, 1, NULL);
-  
+  que_id = osMessageQueueNew(MSGQUEUE_OBJECTS, sizeof(MY_STRUCT), NULL);
   
 }
 
@@ -58,13 +75,17 @@ void Task_init(void)
 void StartTask1(void *argument)
 {
   osTimerStart(tim_periodic, 1000);
-  
-  //char *path = pvPortMalloc(20*sizeof (char));
-  
+	MY_STRUCT msg;
+	
   for(;;)
   {
-    
-    osDelay(1);
+		msg.a = 0x54;
+		msg.b = 0xad21;
+		msg.buff[0] = 0xdd;
+		
+		osMessageQueuePut(que_id, &msg, 0U, 0U);
+		
+    osDelay(500);
   }
   
 }
@@ -74,14 +95,17 @@ void StartTask1(void *argument)
 
 void StartTask2(void *argument)
 {
-  osStatus_t status;
-  osSemaphoreAcquire(sem_tim, osWaitForever);
+  osStatus_t status_sem, staus_que;
+	MY_STRUCT msg;
+	
+  osSemaphoreAcquire(sem_tim, 0);
  
   for(;;)
   {
-    status = osSemaphoreAcquire(sem_tim, osWaitForever);
-    
-    if(status == osOK){
+    status_sem = osSemaphoreAcquire(sem_tim, 0);
+    staus_que = osMessageQueueGet(que_id, &msg, 0U, 0U);
+		
+    if(status_sem == osOK){
       
       char *path = pvPortMalloc(100*sizeof (char));
       
@@ -92,8 +116,12 @@ void StartTask2(void *argument)
       vPortFree(path);
       
     }
+		else if(staus_que == osOK){
+			HAL_UART_Transmit(&huart2, (uint8_t*) &msg, sizeof(msg), 0xffff);
+		}
     
-    osThreadYield(); 
+		osDelay(500);
+    //osThreadYield(); 
   }
   
 }
